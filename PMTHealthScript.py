5eaf3aac4c130943a6658a6a7602bf2feb17ed3a
Caleb Matsick <firstcitizen@terminus.foundation>
2020-08-15 16:43:46 -0400

    Initial commit

---
+++ PMTHealthScript.py
@@ -0,0 +1,13 @@
+# All needed imports
+import os
+import matplotlib.pyplot as plt
+
+
+# Driver method for the script
+def main():
+    print('Beginning.')
+
+
+# Check for main
+if __name__ == '__main__':
+    main()

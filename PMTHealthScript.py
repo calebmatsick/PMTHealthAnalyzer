acebbe2627444d138523cbbee863dfac11458d9c
Caleb Matsick <calebmatsick@gmail.com>
2021-05-12 14:14:01 -0700

    load data into numpy arrays

--- PMTHealthScript.py
+++ PMTHealthScript.py
@@ -10,11 +10,14 @@ from pathlib import Path
 def main():
     dirPath = input('What directory are the value files in?')
 
+    mainArray = np.zeroes((1, 55, 4))
+
     pathlist = Path(dirPath).rglob('*')
     for path in pathlist:
         strPath = str(path)        
+        dataFrame = np.loadtext(strPath, skiprows=1 + 57, dtype=int)
 
-
+        mainArray = np.vstack(dataFrame, mainArray)
 # Check for main
 if __name__ == '__main__':
     main()

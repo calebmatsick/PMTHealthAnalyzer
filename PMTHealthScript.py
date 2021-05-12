49be8a1274f2d4f39dfe9e540a2e903e309c4db5
Caleb Matsick <calebmatsick@gmail.com>
2021-05-12 13:55:30 -0700

    add interation through data files

--- PMTHealthScript.py
+++ PMTHealthScript.py
@@ -1,11 +1,18 @@
 # All needed imports
 import os
 import matplotlib.pyplot as plt
+import numpy as np
+
+from pathlib import Path
 
 
 # Driver method for the script
 def main():
-    print('Beginning.')
+    dirPath = input('What directory are the value files in?')
+
+    pathlist = Path(dirPath).rglob('*')
+    for path in pathlist:
+        strPath = str(path)        
 
 
 # Check for main
